db5159bef0990c1728f67cb081ebf3e5248cc52a
htyuan <htyuan80@gmail.com>
2013-08-22 15:32:06 +0800

    考虑宽高的裁剪

--- src/com/aibang/utils/ImageUtils.java
+++ src/com/aibang/utils/ImageUtils.java
@@ -12,39 +12,99 @@ import android.graphics.RectF;
 
 public class ImageUtils {
 	/**
-     * 将图片截取为圆角图片
-     * @param bitmap 原图片
-     * @param ratio 截取比例，如果是8，则圆角半径是宽高的1/8，如果是2，则是圆形图片
-     * @return 圆角矩形图片
-     */
+	 * 将图片截取为圆角图片
+	 * 
+	 * @param bitmap
+	 *            原图片
+	 * @param ratio
+	 *            截取比例，如果是8，则圆角半径是宽高的1/8，如果是2，则是圆形图片
+	 * @return 圆角矩形图片
+	 */
 	public static Bitmap getRoundedCornerBitmap(Bitmap bitmap, float ratio) {
 		final int w = bitmap.getWidth();
 		final int h = bitmap.getHeight();
-		//create canvas
-		Bitmap output = Bitmap.createBitmap(w,
-				h, Config.ARGB_8888);
+		// create canvas
+		Bitmap output = Bitmap.createBitmap(w, h, Config.ARGB_8888);
 		Canvas canvas = new Canvas(output);
-		
+
 		// init canvas bg
 		canvas.drawARGB(0, 0, 0, 0);
-		
-		//make a rectF
-		
-		//draw a round rect
+
+		// make a rectF
+
+		// draw a round rect
 		final Paint paint = new Paint();
 		paint.setAntiAlias(true);
 		final Rect rect1 = new Rect(0, 0, w, h);
 		final RectF rectF = new RectF(rect1);
 		float rx = w / ratio;
 		float ry = h / ratio;
-		canvas.drawRoundRect(rectF, rx,
-				ry , paint);
+		canvas.drawRoundRect(rectF, rx, ry, paint);
 
-		//draw bitmap with Mode.SRC_IN
+		// draw bitmap with Mode.SRC_IN
 		final Rect rect = new Rect(0, 0, w, h);
 		paint.setXfermode(new PorterDuffXfermode(Mode.SCREEN));
+		// paint.setXfermode(new AvoidXfermode(20, 20,
+		// AvoidXfermode.Mode.TARGET));
 		canvas.drawBitmap(bitmap, rect, rect, paint);
-		
+
+		return output;
+	}
+
+	/**
+	 * 与上面一样，只是考虑到宽搞不同的裁剪
+	 * @param bitmap
+	 * @return
+	 */
+	public Bitmap toRoundBitmap(Bitmap bitmap) {
+		int width = bitmap.getWidth();
+		int height = bitmap.getHeight();
+		float roundPx;
+		float left, top, right, bottom, dst_left, dst_top, dst_right, dst_bottom;
+		if (width <= height) {
+			roundPx = width / 2;
+			top = 0;
+			bottom = width;
+			left = 0;
+			right = width;
+			height = width;
+			dst_left = 0;
+			dst_top = 0;
+			dst_right = width;
+			dst_bottom = width;
+		} else {
+			roundPx = height / 2;
+			float clip = (width - height) / 2;
+			left = clip;
+			right = width - clip;
+			top = 0;
+			bottom = height;
+			width = height;
+			dst_left = 0;
+			dst_top = 0;
+			dst_right = height;
+			dst_bottom = height;
+		}
+
+		Bitmap output = Bitmap.createBitmap(width, height, Config.ARGB_8888);
+		Canvas canvas = new Canvas(output);
+
+		final int color = 0xff424242;
+		final Paint paint = new Paint();
+		final Rect src = new Rect((int) left, (int) top, (int) right,
+				(int) bottom);
+		final Rect dst = new Rect((int) dst_left, (int) dst_top,
+				(int) dst_right, (int) dst_bottom);
+		final RectF rectF = new RectF(dst);
+
+		paint.setAntiAlias(true);
+
+		canvas.drawARGB(0, 0, 0, 0);
+		paint.setColor(color);
+		canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+
+		paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
+		canvas.drawBitmap(bitmap, src, dst, paint);
 		return output;
 	}
 }
